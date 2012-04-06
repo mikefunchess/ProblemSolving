63a133c01a756765731d19e2ccd6b851a45b6679
Thomas Conner <thomas.conner@me.com>
2012-04-06 12:26:45 -0400

    Adding your shit.

--- ProblemSolvingProject/Form1.cs
+++ ProblemSolvingProject/Form1.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        //private System.Drawing.Bitmap myBitmap; // Our Bitmap declaration
+     //private System.Drawing.Bitmap myBitmap; // Our Bitmap declaration
         
         public Form1()
         {
@@ -20,9 +20,12 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label12.Hide();
-            textBox1.Hide();
-            button2.Hide();
+            Move_ex.Hide();
+            Move_enter.Hide();
+            move_button.Hide();
+            Turn_pick.Hide();
+            Player_first.Hide();
+            Comp_first.Hide();
 
             //this.pictureBox1.Location = new Point(0, 0);
             
@@ -67,12 +70,13 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             //radioButton1.Dispose();
-            radioButton2.Dispose();
-            radioButton3.Dispose();
-            button1.Dispose();
-            label12.Show();
-            textBox1.Show();
-            button2.Show();
+            Red_choice.Dispose();
+            Green_choice.Dispose();
+            color_pick.Dispose();
+            Turn_pick.Show();
+            Player_first.Show();
+            Comp_first.Show();
+
         }
 
         private void pictureBox8_Click(object sender, EventArgs e)
@@ -84,12 +88,88 @@ namespace WindowsFormsApplication1
         {
             //48, 40 are the starting coordinates of a1.
             //add 48 to each in order to move to the next square.
-
+            //MessageBox.Show(Move_enter.Text);
+            string move = Move_enter.Text;
+            char index = move[0];
+            int i = 40 + (48 * (index - 'a'));
+            int j = (int)Char.GetNumericValue(move[1]);
+            j = j * 48;
+            index = move[2];
+            int k = 40 + (48 * (index - 'a'));
+            int l = (int)Char.GetNumericValue(move[3]);
+            l = l * 48;
+            Point check = new Point (j, i);
+            
+            MessageBox.Show("Moving "+move);
+            if(Green1.Location == check )
+            {
+                Green1.Location = new Point(l, k);
+            }
+            else if (Green2.Location == check)
+            {
+                Green2.Location = new Point(l, k);
+            }
+            else if (Green3.Location == check)
+            {
+                Green3.Location = new Point(l, k);
+            }
+            else if (Green4.Location == check)
+            {
+                Green4.Location = new Point(l, k);
+            }
+            else if (Green5.Location == check)
+            {
+                Green5.Location = new Point(l, k);
+            }
+            else if (Green6.Location == check)
+            {
+                Green6.Location = new Point(l, k);
+            }
+            else if (Green7.Location == check)
+            {
+                Green7.Location = new Point(l, k);
+            }
+            else if (Red1.Location == check)
+            {
+                Red1.Location = new Point(l, k);
+            }
+            else if (Red2.Location == check)
+            {
+                Red2.Location = new Point(l, k);
+            }
+            else if (Red3.Location == check)
+            {
+                Red3.Location = new Point(l, k);
+            }
+            else if (Red4.Location == check)
+            {
+                Red4.Location = new Point(l, k);
+            }
+            else if (Red5.Location == check)
+            {
+                Red5.Location = new Point(l, k);
+            }
+            else if (Red6.Location == check)
+            {
+                Red6.Location = new Point(l, k);
+            }
+            else if (Red7.Location == check)
+            {
+                Red7.Location = new Point(l, k);
+            }
+            //move[0]
+            /*
+             * 
+             * 
+             * 
+             * 
+             * ***********************************************************************************/
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void move_enter_TextChanged(object sender, EventArgs e)
         {
             //move text box
+            //string move = Move_enter.Text;
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -97,5 +177,19 @@ namespace WindowsFormsApplication1
 
         }
 
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Turn_pick_Click(object sender, EventArgs e)
+        {
+            Turn_pick.Dispose();
+            Player_first.Dispose();
+            Comp_first.Dispose();
+            Move_ex.Show();
+            Move_enter.Show();
+            move_button.Show();
+        }
     }
 }
